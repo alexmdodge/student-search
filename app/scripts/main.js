d4e80669cf1cdab452b3a9942840e3cbcacc7429
alexmdodge <alex.mdodge@gmail.com>
2016-09-10 15:05:57 -0300

    Logic Setup This commit re-organizes the file and removes a lot of repeated functionality, placing it into objects and other functions.
    The variable names for determing which search options were being used also are changed to help with readability throughout the script.
    
    The query function is better explained, and the submit button is connected to the clicking of the search boxes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,86 @@
 "use strict";
-// Load the Visualization API and the piechart package.
-google.load("visualization", "1", {packages:["table"]});
 
-// Test URL - Note that this URL is unsecure, so anyone can access the spreadsheet.
-// https://docs.google.com/spreadsheets/d/1ABtCqLWs0AJSpwpXo6stMZgs6pk4yyQilkjcfSDRH30/edit?usp=sharing
+/* * * * JSHint Global Definitions * * */
+/* globals $, google, console, alert   */
+/* * * * * * * * * * * * * * * * * * * */
 
-// This function
-function init() {
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *                          Student Query Application
+ *                     Author: Alex Dodge |  License: MIT
+ *                             
+ *                               Version 0.0.0
+ *                             September 10, 2016
+ *
+ *
+ *  
+ *  This is a querying application designed to retrieve student information
+ *  from a Google sheet generated from a Google form. The visualization library
+ *  was preferred as the teacher wanted access to the database in a simple way.
+ *
+ *  For security purposes it is not recommended to use this application in an
+ *  easily accessible web space. It is preferable to have it only accessible
+ *  through authentication. This is due to the fact that the spreadsheet is
+ *  accessible in the JavaScript code, and if someone has the link they can see
+ *  any information in the sheet.
+ *
+ *  For more information check the GitHub repository!
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+/* Load the Google Visualization API and the pie chart package.
+ * The google object is the js api loading script defined earlier in 
+ * the project.
+ */
+google.load("visualization", "1", {packages:["table", "piechart"]});
+
+// Debugging is enabled by default, and debug messages will be displayed in
+// the console window.
+var debugging = true;
+
+
+
+/* 
+ * function: init
+ * parameters: queryState
+ * description: Initiates the query response which will be sent and retrieved
+ *              using the Google Visualization API.
+ *
+ * Parameter def: 
+ *    queryState --> This parameter is an object which contains all of the
+ *                   validated fields from the submitted query. It uses the
+ *                   information about which boxes were clicked and which
+ *                   information was entered.
+ *
+ *
+ */
+function init(queryState) {
+
+   // Set default functionality for the query request
    var opts = {
       sendMethod: 'auto'
    };
 
-   // Retrieve all inputs and store them in variables
-   var urlString = $('#google-url').val();
-   var queryString = $('#query-request').val();
+   /* Retrieve all inputs and store them in variables
+    * The current URL is a test sheet which was constructed using
+    * the dummy data
+    *
+    * For a user input query string
+    * var urlString = $('#google-url').val();
+    */
 
-   console.log(urlString);
-   console.log(queryString);
+   var urlString = "https://docs.google.com/spreadsheets/d/1ABtCqLWs0AJSpwpXo6stMZgs6pk4yyQilkjcfSDRH30/edit?usp=sharing";
+   var queryString = $('#student-query').val();
+
+   // Use these statements to ensure queries are workin properly and values are retrieved
+   if (debugging) {
+      console.log(queryString);
+      console.log(urlString);
+   }
 
    var query = new google.visualization.Query(urlString, opts);
    query.setQuery(queryString);
@@ -40,39 +103,60 @@ function handleQueryResponse(response) {
    $('#csv-output').append(csv);
 }
 
+function enableSubmit() {
+   if( $('.submit-query').hasClass('disabled') ) {
+      $('.submit-query').removeClass('disabled');
+   }
+}
+
+function disableSubmit(student, school, amount) {
+   if( !$('.submit-query').hasClass('disabled') ) {
+      if( !student && !school &&  !amount)
+      $('.submit-query').addClass('disabled');
+   }
+}
+
 $(document).ready(function() {
-   var studentStatus = false;
-   var schoolStatus = false;
-   var amountStatus = false;
+   var isStudentChecked = false;
+   var isSchoolChecked = false;
+   var isAmountChecked = false;
 
    $('#student-checkbox').click(function() {
       if ( $('#student-checkbox').prop('checked') ) {
          $('.name-filter-container').slideDown('fast');
          $('.submit-button').fadeIn('fast');
-         studentStatus = true;
+         isStudentChecked = true;
+         enableSubmit();
+         
       } else {
          $('.name-filter-container').slideUp('fast');
-         studentStatus = false;
+         isStudentChecked = false;
+         disableSubmit(isStudentChecked, isSchoolChecked, isAmountChecked);
       }
    });
 
    $('#school-checkbox').click(function() {
       if ( $('#school-checkbox').prop('checked') ) {
          $('.school-filter-container').slideDown('fast');
-         schoolStatus = true;
+         isSchoolChecked = true;
+         enableSubmit();
+         
       } else {
          $('.school-filter-container').slideUp('fast');
-         schoolStatus = false;
+         isSchoolChecked = false;
+         disableSubmit(isStudentChecked, isSchoolChecked, isAmountChecked);
       }
    });
 
    $('#amount-checkbox').click(function() {
       if ( $('#amount-checkbox').prop('checked') ) {
          $('.amount-filter-container').slideDown('fast');
-         amountStatus = true;
+         isAmountChecked = true;
+         enableSubmit();
       } else {
          $('.amount-filter-container').slideUp('fast');
-         amountStatus = false;
+         isAmountChecked = false;
+         disableSubmit(isStudentChecked, isSchoolChecked, isAmountChecked);
       }
    });
 
